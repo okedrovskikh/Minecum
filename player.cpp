b8da3969c135356e186bfc037260c5eef12e4851
ut1Puti <91106828+ut1Puti@users.noreply.github.com>
2022-04-03 14:02:30 +0300

    Add files via upload

--- player.cpp
+++ player.cpp
@@ -5,10 +5,10 @@ Player::Player()
 
 }
 
-Player::Player(glm::vec3 position, unsigned int& VAO, unsigned int& VBO) :
-	camera(glm::vec3(position.x, position.y + HEIGHT_Y / 2, position.z))
+Player::Player(glm::vec3 position, unsigned int& VAO, unsigned int& VBO) : camera(glm::vec3(position.x, position.y + HEIGHT_Y / 2 - 0.13f, position.z))
 {
 	this->position = position;
+
 	state = MIDAIR;
 
 	glGenVertexArrays(1, &VAO);
@@ -29,7 +29,7 @@ Player::Player(glm::vec3 position, unsigned int& VAO, unsigned int& VBO) :
 	glEnableVertexAttribArray(2);
 }
 
-void Player::processMovement(GLFWwindow* window, float deltaTime, Chunk v)
+void Player::processMovement(GLFWwindow* window, float deltaTime, std::vector<glm::vec3> v)
 {
 	glm::vec3 motion = glm::vec3(0.0f);
 
@@ -73,54 +73,81 @@ void Player::processMovement(GLFWwindow* window, float deltaTime, Chunk v)
 	applyMotion(motion, v);
 }
 
+void Player::updateCamera()
+{
+	camera.update(glm::vec3(position.x, position.y + HEIGHT_Y / 2 - 0.13f, position.z));
+}
+
 bool Player::AABB(float one, float two)
 {
 	return (one - (two + 0.5f)) * (one - (two - 0.5f)) <= 0;
 }
 
-void Player::applyMotion(glm::vec3 motion, Chunk v)
+bool Player::AABB_(float one, float two, float par)
+{
+	return (one - (two + par)) * (one - (two - par)) <= 0;
+}
+
+void Player::applyMotion(glm::vec3 motion, std::vector<glm::vec3> v)
 {
 	if (motion.y <= -1.0f)
 		motion.y = -1.0f;
 	
 	glm::vec3 newPosition = position + motion;
-	glm::vec3 perpendicularMotion = glm::vec3(-motion.z, 0.0f, motion.x);
 
-	glm::vec3 top = glm::vec3(newPosition.x + 0.3f, newPosition.y + 0.3f, newPosition.z + 0.3f);
-	glm::vec3 bottom = glm::vec3(newPosition.x - 0.3f, newPosition.y - 0.3f, newPosition.z - 0.3f);
+	glm::vec3 top = glm::vec3(newPosition.x + WIDTH_X / 2, newPosition.y + HEIGHT_Y / 2 , newPosition.z + WIDTH_Z / 2);
+	glm::vec3 bottom = glm::vec3(newPosition.x - WIDTH_X / 2, newPosition.y - HEIGHT_Y / 2, newPosition.z - WIDTH_Z / 2);
 
+	bool collisionOnBottom = false;
+	bool collisionOnSide = false;
 	bool overallColliding = false;
 
-
-	for (int i = 0; i < v.coordinate.size(); i++)
+	int size = v.size();
+	for (int i = 0; i < size; i++)
 	{
-		glm::vec3 coord = v.coordinate[i];
-		bool collidingX = AABB(top.x, coord.x) || AABB(bottom.x, coord.x);
-		bool collidingY = AABB(top.y, coord.y) || AABB(bottom.y, coord.y);
-		bool collidingZ = AABB(top.z, coord.z) || AABB(bottom.z, coord.z);
+		glm::vec3 coord = v[i];
+		//std::cout << coord.x << " " << coord.y << " " << coord.z << std::endl;
+		bool collidingX = AABB(top.x, coord.x) || AABB(bottom.x, coord.x) || AABB_(coord.x, newPosition.x) ;
+		bool collidingY = AABB(top.y, coord.y) || AABB(bottom.y, coord.y) || AABB_(coord.y, newPosition.y, HEIGHT_Y / 2);
+		bool collidingZ = AABB(top.z, coord.z) || AABB(bottom.z, coord.z) || AABB_(coord.z, newPosition.z);
 		bool colliding = collidingX && collidingY && collidingZ;
 		if (colliding) {
-			std::cout << "colliding" << std::endl;
 			overallColliding = true;
+			//error somewhere in that if/else cicle
+			//probably cause newPosition's y should be in range of coord's y
 			if (coord.y + 0.5f < newPosition.y) {
 				state = STANDING;
 				yVelocity = 0.0f;
-				std::cout << "Collision on bottom" << std::endl;
+				std::cout << "Collision on bottom " << coord.x << " " << coord.y << " " << coord.z << std::endl;
 				newPosition.y = round(ceil(position.y * 100) / 10) / 10;
+				collisionOnBottom = true;
 			}
-			if ((coord.y - 0.5f <= newPosition.y && newPosition.y <= coord.y + 0.5f)) {
-				std::cout << "Coliision on side" << std::endl;
-				if (coord.x - 0.5f <= newPosition.x && newPosition.x <= coord.x + 0.5f)
-					newPosition.z = round(ceil(position.z * 100) / 10) / 10;
-				if (coord.z - 0.5f <= newPosition.z && newPosition.z <= coord.z + 0.5f)
-					newPosition.x = round(ceil(position.x * 100) / 10) / 10;
-			}
-			if (coord.y - 0.5f > newPosition.y) {
+			else if (coord.y - 0.5f >= newPosition.y) {
 				std::cout << "Collision on top" << std::endl;
 				newPosition.y = round(ceil(position.y * 100) / 10) / 10;
 				state = MIDAIR;
 			}
+			if (coord.y - 0.5f <= newPosition.y && newPosition.y <= coord.y + 0.5f) {
+				std::cout << "Collision on side" << std::endl;
+				if (coord.x - 0.5f <= newPosition.x && newPosition.x <= coord.x + 0.5f)
+					newPosition.z = round(ceil(position.z * 100) / 10) / 10;
+				else if (coord.z - 0.5f <= newPosition.z && newPosition.z < coord.z + 0.5f)
+					newPosition.x = round(ceil(position.x * 100) / 10) / 10;
+				collisionOnSide = true;
+			}
+			else if (newPosition.y - HEIGHT_Y / 2 <= coord.y && coord.y <= newPosition.y + HEIGHT_Y / 2) {
+				std::cout << "Collision on side" << std::endl;
+				if (coord.x - 0.5f <= newPosition.x && newPosition.x <= coord.x + 0.5f)
+					newPosition.z = round(ceil(position.z * 100) / 10) / 10;
+				else if (coord.z - 0.5f <= newPosition.z && newPosition.z < coord.z + 0.5f)
+					newPosition.x = round(ceil(position.x * 100) / 10) / 10;
+				collisionOnSide = true;
+			}
 		}
+	}
+
+	if (collisionOnSide && !collisionOnBottom) {
+		state = MIDAIR;
 	}
 
 	if (!overallColliding) {
@@ -134,4 +161,9 @@ void Player::applyMotion(glm::vec3 motion, Chunk v)
 Player::~Player()
 {
 
+}
+
+float distance(glm::vec3 p1, glm::vec3 p2)
+{
+	return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.x - p2.z));
 }
--- player.h
+++ player.h
@@ -3,8 +3,6 @@
 #include "camera.h"
 #include "texture.h"
 #include "block.h"
-#include "chunk.h"
-#include <limits>
 #include <vector>
 
 const float WIDTH_X = 0.6f;
@@ -22,8 +20,8 @@ public:
 	Camera camera;
 	Player();
     Player(glm::vec3 position, unsigned int& VAO, unsigned int& VBO);
-    void processMovement(GLFWwindow* window, float deltaTime, Chunk v);
-    bool AABB(float one, float two);
+    void processMovement(GLFWwindow* window, float deltaTime, std::vector<glm::vec3> v);
+    void updateCamera();
     ~Player();
 private:
     float yVelocity = 0.0f;
@@ -71,5 +69,9 @@ private:
         -0.3f,  0.3f,  0.3f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
         -0.3f,  0.3f, -0.3f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f
     };
-    void applyMotion(glm::vec3 motion, Chunk v);
+    void applyMotion(glm::vec3 motion, std::vector<glm::vec3> v);
+    bool AABB(float one, float two);
+    bool AABB_(float one, float two, float par = 0.3f);
 };
+
+float distance(glm::vec3 p1, glm::vec3 p2);
